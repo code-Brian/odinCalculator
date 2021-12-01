e5024b2d2f01aaefe2cffb59de58fb05848165c5
Brian Denmark <testingarea479@gmail.com>
2021-12-01 11:09:39 -0800

    updated operate function to call separate math functions and evaluate to result variable

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // query selectors here
 const display = document.querySelector('#display');
 
-// good placeholder code for a basic concept... might use later, will likely delete
-/** let operator = prompt("Select your operator: add, subtract, divide, or multiply", '');
-
+// this switch block may no longer be necessary... will re-evaluate later.
+/** 
  switch (operator) {
     case 'add': 
         operator = 'add';
@@ -17,39 +16,53 @@ const display = document.querySelector('#display');
     case 'divide':
         operator = 'divide';
         break;
-}
+} */
 
-currentNum = 20;
-nextNum = 2;
 let result = 0;
 
-function operate(operator, currentNum, nextNum) {
+function operate(operator, a, b) {
 
     switch(operator) {
         case 'add':
-            result = currentNum + nextNum;
+            result = add(a,b);
             break;
         case 'subtract': 
-            result = currentNum - nextNum;
+            result = subtract(a,b);
             break;
         case 'multiply':
-            result = currentNum * nextNum;
+            result = multiply(a,b);
             break;
         case 'divide': 
-            if (nextNum === 0) {
-                throw  'Cannot divide by zero. Do you want the universe to explode!??!?';
-            }
-            result = currentNum / nextNum;
+            result = divide(a,b);
             break;
     }
 
     return result;
 
-} **/
+} 
 
+function add(a,b) {
+    let sum = a + b;
+    return sum;
+}
 
+function subtract(a,b) {
+    let difference = a - b;
+    return difference;
+}
 
+function multiply(a,b) {
+    let product = a * b;
+    return product;
+}
 
-
+function divide(a, b) {
+    if ( currentNum != 0) {
+        let quotient = a / b;
+        return quotient;
+    } else {
+        throw 'Cannot divide by zero; The universe would implode.';
+    }
+}
 
 // event listeners here
