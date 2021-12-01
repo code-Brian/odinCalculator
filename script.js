edef5779ab86aae89e89d8cbf4d5ffd1e24c619c
Brian Denmark <testingarea479@gmail.com>
2021-12-01 15:08:44 -0800

    rough functionality being incorporated, still not perfect, will make further adjustments

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
+let displayValue = "0";
+
 // query selectors here
 const display = document.querySelector('#display');
+display.textContent = displayValue;
 
-// this switch block may no longer be necessary... will re-evaluate later.
-/** 
- switch (operator) {
-    case 'add': 
-        operator = 'add';
-        break;
-    case 'subtract': 
-        operator = 'subtract';
-        break;
-    case 'multiply': 
-        operator = 'multiply';
-        break;
-    case 'divide':
-        operator = 'divide';
-        break;
-} */
-
+let operator;
 let result = 0;
 
 function operate(operator, a, b) {
@@ -57,7 +44,7 @@ function multiply(a,b) {
 }
 
 function divide(a, b) {
-    if ( currentNum != 0) {
+    if ( b != 0) {
         let quotient = a / b;
         return quotient;
     } else {
@@ -66,3 +53,116 @@ function divide(a, b) {
 }
 
 // event listeners here
+// All numeric input values
+const zero = document.getElementById('zero');
+zero.addEventListener('mouseup', () => {
+    if (displayValue.charAt(0) != "0") {
+        displayValue += "0";
+    }
+    
+    display.textContent = displayValue;
+});
+
+const one = document.getElementById('one');
+one.addEventListener('mouseup', () => {
+    displayValue += "1";
+    display.textContent = displayValue;
+});
+
+const two = document.getElementById('two');
+two.addEventListener('mouseup', () => {
+    displayValue += "2";
+    display.textContent = displayValue;
+});
+
+const three = document.getElementById('three');
+three.addEventListener('mouseup', () => {
+    displayValue += "3";
+    display.textContent = displayValue;
+});
+
+const four = document.getElementById('four');
+four.addEventListener('mouseup', () => {
+    displayValue += "4";
+    display.textContent = displayValue;
+});
+
+const five = document.getElementById('five');
+five.addEventListener('mouseup', () => {
+    displayValue += "5";
+    display.textContent = displayValue;
+});
+
+const six = document.getElementById('six');
+six.addEventListener('mouseup', () => {
+    displayValue += "6";
+    display.textContent = displayValue;
+});
+
+const seven = document.getElementById('seven');
+seven.addEventListener('mouseup', () => {
+    displayValue += "7";
+    display.textContent = displayValue;
+});
+
+const eight = document.getElementById('eight');
+eight.addEventListener('mouseup', () => {
+    displayValue += "8";
+    display.textContent = displayValue;
+});
+
+const nine = document.getElementById('nine');
+nine.addEventListener('mouseup', () => {
+    displayValue += "9";
+    display.textContent = displayValue;
+});
+
+//operator value inputs
+const divChar = document.getElementById('divide');
+divChar.addEventListener('mouseup', () => {
+    displayValue = '÷';
+    display.textContent = displayValue;
+});
+
+const multChar = document.getElementById('multiply');
+multChar.addEventListener('mouseup', () => {
+    displayValue = '×';
+    operator = 'multiply';
+    display.textContent = displayValue;
+});
+
+const subtChar = document.getElementById('subtract');
+subtChar.addEventListener('mouseup', () => {
+    displayValue = '−';
+    operator = 'subtract';
+    display.textContent = displayValue;
+});
+
+const addChar = document.getElementById('add');
+addChar.addEventListener('mouseup', () => {
+    displayValue = '+';
+    operator = 'add';
+    display.textContent = displayValue;
+});
+
+const deciChar = document.getElementById('decimal');
+deciChar.addEventListener('mouseup', () => {
+    displayValue = '∙';
+    // will add operator later
+    display.textContent = displayValue;
+})
+
+const equals = document.getElementById('equals');
+equals.addEventListener('mouseup', () => {
+    operate(operator, a, b);
+    display.textContent = result;
+})
+
+const clear = document.getElementById('clear');
+clear.addEventListener('mouseup', () => {
+    displayValue = 0;
+    a = 0;
+    b = 0;
+    operator = null;
+    display.textContent = displayValue; 
+});
