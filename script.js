3f0e086fcf11ecaca31ba2d918db06cb87e9492e
Bastion <testingarea479@gmail.com>
2022-05-17 14:19:52 -0700

    core functionality updated to display some basic operations on the screen. Further coding in progress

--- script.js
+++ script.js
@@ -1,68 +1,110 @@
-const numButtons = document.querySelectorAll('[data-number]');
-
-numButtons.forEach((button) =>
-    button.addEventListener('click', () => appendNumber(button.textContent))   
-)
-
-let operator;
+let firstOperand = '';
+let secondOperand = '';
 let currentOperation = null;
 let shouldResetScreen = false;
 
+const numButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const clearButton = document.getElementById('clearBtn');
+const deleteButton = document.getElementById('deleteBtn');
+const decimalButton = document.getElementById('decimalBtn');
+const equalsButton = document.getElementById('equalsBtn');
+const previousOperationScreen = document.getElementById('previousOperationScreen');
+const currentOperationScreen = document.getElementById('currentOperationScreen');
+
+clearButton.addEventListener('click', clear);
+deleteButton.addEventListener('click', deleteNumber);
+decimalButton.addEventListener('click', appendDecimal);
+equalsButton.addEventListener('click', evaluate);
+
+
+numButtons.forEach((button) =>
+    button.addEventListener('click', () => appendNumber(button.textContent))   
+);
+
+operatorButtons.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+);
+
+function appendDecimal() {
+
+}
+
+function deleteNumber() {
+    // placeholder code
+}
+
+function clear() {
+    currentOperationScreen.textContent = '0';
+    previousOperationScreen.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperation = null;
+}
 function appendNumber(number) {
-    if (currentOperationScreen.textContent === '0' || shouldResetScreen)
-      resetScreen()
+    if (currentOperationScreen.textContent === '0' || shouldResetScreen) resetScreen()
     currentOperationScreen.textContent += number
     console.log(currentOperationScreen.textContent)
   }
+
+function setOperation(operator) {
+    if (currentOperation !== null) evaluate();
+    firstOperand = currentOperationScreen.textContent;
+    currentOperation = operator;
+    previousOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+    shouldResetScreen = true;
+}
+
+function evaluate() {
+    if (currentOperation === null || shouldResetScreen) return;
+    if (currentOperation === '/' && currentOperationScreen === '0') {
+        alert("You can't divide by 0! The Universe would explode!");
+        return
+    }
+    secondOperand = currentOperationScreen.textContent;
+    currentOperationScreen.textContent = operate(currentOperation, firstOperand, secondOperand);
+    previousOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+    currentOperation = null;
+}
 
 function resetScreen() {
     currentOperationScreen.textContent = '';
     shouldResetScreen = false;
 }
 function operate(operator, a, b) {
-
+    a = Number(a);
+    b = Number(b);
     switch(operator) {
-        case 'add':
-            result = add(a,b);
-            break;
-        case 'subtract': 
-            result = subtract(a,b);
-            break;
-        case 'multiply':
-            result = multiply(a,b);
-            break;
-        case 'divide': 
-            result = divide(a,b);
-            break;
+        case '+':
+            return add(a,b);
+        case '-': 
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '/': 
+            return divide(a,b);
+        default:
+            return null;
     }
-
-    return result;
-
-} 
+    
+}
 
 function add(a,b) { 
-    let sum = a + b;
-    return sum;
+    return a + b;
 }
 
 function subtract(a,b) {
-    let difference = a - b;
-    return difference;
+    return a - b;
 }
 
 function multiply(a,b) {
-    let product = a * b;
-    return product;
+    return a * b;
 }
 
 function divide(a, b) {
-    if ( b != 0) {
-        let quotient = a / b;
-        return quotient;
-    } else {
-        throw 'Cannot divide by zero; The universe would implode.';
-    }
+    return a / b;
 }
+
 // this code snippet will automatically add commas as needed :)
 // function numberWithCommas(x) {
 //  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
